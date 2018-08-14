b3cf611db14569f94623d87474cf5aef7aab74ab
Joshua Barnett <joecoolatbyu@ldschurch.org>
2018-08-14 12:59:16 -0600

    ignore case

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
-    List<Location> findByStateStartingWith(String sateName);
+    List<Location> findByStateIgnoreCaseStartingWith(String sateName);
     List<Location> findByStateNotLike(String sateName);
     List<Location> findByStateIsOrCountryEquals(String value, String value2);
     List<Location> findByStateNot(String state);
