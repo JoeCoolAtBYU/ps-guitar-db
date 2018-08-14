0dbcdddb218280b8d842363693df1fb2db07fe00
Joshua Barnett <joecoolatbyu@ldschurch.org>
2018-08-14 14:15:19 -0600

    @query annotation

--- src/main/java/com/guitar/db/repository/ModelRepository.java
+++ src/main/java/com/guitar/db/repository/ModelRepository.java
@@ -13,77 +13,72 @@ import java.util.List;
 
 @Repository
 public class ModelRepository {
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Autowired
-	private ModelJpaRepository modelJpaRepository;
-	/**
-	 * Create
-	 */
-	public Model create(Model mod) {
-		return modelJpaRepository.saveAndFlush(mod);
-	}
+    @Autowired
+    private ModelJpaRepository modelJpaRepository;
 
-	/**
-	 * Update
-	 */
-	public Model update(Model mod) {
-		return modelJpaRepository.saveAndFlush(mod);
-	}
+    /**
+     * Create
+     */
+    public Model create(Model mod) {
+        return modelJpaRepository.saveAndFlush(mod);
+    }
 
-	/**
-	 * Delete
-	 */
-	public void delete(Model mod) {
-		modelJpaRepository.delete(mod);
-	}
+    /**
+     * Update
+     */
+    public Model update(Model mod) {
+        return modelJpaRepository.saveAndFlush(mod);
+    }
 
-	/**
-	 * Find
-	 */
-	public Model find(Long id) {
-		return modelJpaRepository.findOne(id);
-	}
+    /**
+     * Delete
+     */
+    public void delete(Model mod) {
+        modelJpaRepository.delete(mod);
+    }
 
-	/**
-	 * Custom finder
-	 */
-	public List<Model> getModelsInPriceRange(BigDecimal lowest, BigDecimal highest) {
-		return modelJpaRepository.findByPriceGreaterThanEqualAndPriceLessThanEqual(lowest, highest);
-	}
+    /**
+     * Find
+     */
+    public Model find(Long id) {
+        return modelJpaRepository.findOne(id);
+    }
 
-	/**
-	 * Custom finder
-	 */
-	public List<Model> getModelsByPriceRangeAndWoodType(BigDecimal lowest, BigDecimal highest, String wood) {
-		@SuppressWarnings("unchecked")
-		List<Model> mods = entityManager
-				.createQuery("select m from Model m where m.price >= :lowest and m.price <= :highest and m.woodType like :wood")
-				.setParameter("lowest", lowest)
-				.setParameter("highest", highest)
-				.setParameter("wood", "%" + wood + "%").getResultList();
-		return mods;
-	}
+    /**
+     * Custom finder
+     */
+    public List<Model> getModelsInPriceRange(BigDecimal lowest, BigDecimal highest) {
+        return modelJpaRepository.findByPriceGreaterThanEqualAndPriceLessThanEqual(lowest, highest);
+    }
 
-	/**
-	 * NamedQuery finder
-	 */
-	public List<Model> getModelsByType(String modelType) {
-		@SuppressWarnings("unchecked")
-		List<Model> mods = entityManager
-				.createNamedQuery("Model.findAllModelsByType")
-				.setParameter("name", modelType).getResultList();
-		return mods;
-	}
+    /**
+     * Custom finder
+     */
+    public List<Model> getModelsByPriceRangeAndWoodType(BigDecimal lowest, BigDecimal highest, String wood) {
+        return modelJpaRepository.queryByPriceRangeAndWoodType(lowest, highest, "%" + wood + "%");
+    }
 
-	/**
-	 * Count
-	 */
-	public Long getModelCount() {
-		CriteriaBuilder qb = entityManager.getCriteriaBuilder();
-		CriteriaQuery<Long> cq = qb.createQuery(Long.class);
-		cq.select(qb.count(cq.from(Model.class)));
-		return entityManager.createQuery(cq).getSingleResult();
-	}
+    /**
+     * NamedQuery finder
+     */
+    public List<Model> getModelsByType(String modelType) {
+        @SuppressWarnings("unchecked")
+        List<Model> mods = entityManager
+                .createNamedQuery("Model.findAllModelsByType")
+                .setParameter("name", modelType).getResultList();
+        return mods;
+    }
+
+    /**
+     * Count
+     */
+    public Long getModelCount() {
+        CriteriaBuilder qb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Long> cq = qb.createQuery(Long.class);
+        cq.select(qb.count(cq.from(Model.class)));
+        return entityManager.createQuery(cq).getSingleResult();
+    }
 }
